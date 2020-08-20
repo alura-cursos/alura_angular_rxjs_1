6c58305568ed4f76b94cfa7e716d694bd75c57df
Alvaro <alvaro.camilloneto@gmail.com>
2020-08-20 08:49:31 -0300

    Aular 2.1

--- byte-bank-broker/src/app/acoes/acoes.service.ts
+++ byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Acao } from './modelo/acoes';
 
 @Injectable({
   providedIn: 'root',
@@ -8,6 +10,24 @@ export class AcoesService {
   constructor(private httpClient: HttpClient) {}
 
   getAcoes() {
-    return this.httpClient.get<any>('http://localhost:3000/acoes');
+    return this.httpClient
+      .get<any>('http://localhost:3000/acoes')
+      .pipe(
+        map((acoes) =>
+          acoes.sort((acaoA, acaoB) => this.ordenaPorCodigo(acaoA, acaoB))
+        )
+      );
+  }
+
+  private ordenaPorCodigo(acaoA: Acao, acaoB: Acao) {
+    if (acaoA.codigo > acaoB.codigo) {
+      return 1;
+    }
+
+    if (acaoA.codigo < acaoB.codigo) {
+      return -1;
+    }
+
+    return 0;
   }
 }
