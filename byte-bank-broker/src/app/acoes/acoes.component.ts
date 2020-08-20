ea52e0e859598b97204cba069712a084c41dea56
Alvaro <alvaro.camilloneto@gmail.com>
2020-08-20 08:04:52 -0300

    Aula 1.3

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Acoes } from './modelo/acoes';
+import { AcoesService } from './acoes.service';
 
 @Component({
   selector: 'app-acoes',
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
-export class AcoesComponent {
+export class AcoesComponent implements OnInit {
   acoesInput = new FormControl();
+  acoes: Acoes;
 
-  constructor() {}
+  constructor(private acoesService: AcoesService) {}
+
+  ngOnInit() {
+    this.acoesService.getAcoes().subscribe((retornoApi) => {
+      this.acoes = retornoApi.payload;
+    });
+  }
 }
