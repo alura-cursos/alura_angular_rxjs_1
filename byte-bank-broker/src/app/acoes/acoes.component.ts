9e8089faf302de67442c450aa1d34659b8b998b5
Alvaro <alvaro.camilloneto@gmail.com>
2020-08-21 08:15:38 -0300

    Aular 2.4

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -9,20 +9,9 @@ import { Subscription } from 'rxjs';
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
-export class AcoesComponent implements OnInit, OnDestroy {
+export class AcoesComponent {
   acoesInput = new FormControl();
-  acoes: Acoes;
-  private subscription: Subscription;
+  acoes$ = this.acoesService.getAcoes();
 
   constructor(private acoesService: AcoesService) {}
-
-  ngOnInit() {
-    this.subscription = this.acoesService.getAcoes().subscribe((acoes) => {
-      this.acoes = acoes;
-    });
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
 }
